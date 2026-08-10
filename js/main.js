a4c435feb9969e90af33ae97cfe540b91749390a
SlayerArman <cartoontv123360@gmail.com>
2026-08-10 16:51:38 +0500

    build main menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 console.log("Alone at 3AM");
 
 const app = document.querySelector('#app');
+const menuButtons = document.querySelectorAll("[data-action]");
 
 console.log("Application loaded:", app !== null);
+
+menuButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const action = button.dataset.action;
+
+        console.log(`Menu action: ${action}`);
+    });
+});
